649e920a0c1a30ddfa32567eec17c624157bb584
Asish Kumar Das <asishdas370@gmail.com>
2026-07-31 14:01:30 +0530

    Add presence logging

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -1,11 +1,16 @@
 module.exports = client => {
-    const updateMemberCount = async () => {
-        const guild = client.guilds.cache.get('1532032501906800822');
+    const updateMemberCount = () => {
+        const guild = client.guilds.cache.get("1532032501906800822");
+
         setInterval(() => {
             const memberCount = guild.memberCount;
-            const channel = guild.channels.cache.get('1532032502636740861');
-            channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
-            console.log(`Updated member count to ${memberCount}`);
-        }, 5000); 
-    }
-}
+            const channel = guild.channels.cache.get("1532032502636740861");
+
+            if (channel) {
+                channel.setName(`Total Members: ${memberCount}`);
+            }
+        }, 5000);
+    };
+
+    updateMemberCount();
+};
